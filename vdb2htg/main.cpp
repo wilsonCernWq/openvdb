a2d0a3efb8c1ca84242c744306e7dc3294587112
WU Qi <wilson.over.cloud@gmail.com>
2020-08-09 15:20:16 -0700

    running on linux

--- vdb2htg/main.cpp
+++ vdb2htg/main.cpp
@@ -1,59 +1,84 @@
 #include <iostream>
 #include "openvdb/openvdb.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     std::cout << "hello" << std::endl;
-    
+
     openvdb::initialize();
     openvdb::logging::initialize(argc, argv);
 
     // Create a VDB file object.
-    std::string name = "E:\\vdb\\v224\\sphere_points.vdb-1.0.0\\sphere_points.vdb";
+    //std::string name = "E:\\vdb\\v224\\sphere_points.vdb-1.0.0\\sphere_points.vdb";
+    std::string name = "/home/qadwu/Data/openvdb/bunny_cloud.vdb";
     openvdb::io::File file(name);
     // Open the file.  This reads the file header, but not any grids.
-    // file.open();
-    // // Loop over all grids in the file and retrieve a shared pointer
-    // // to the one named "LevelSetSphere".  (This can also be done
-    // // more simply by calling file.readGrid("LevelSetSphere").)
-    // openvdb::GridBase::Ptr baseGrid;
-    // for (openvdb::io::File::NameIterator nameIter = file.beginName();
-    //     nameIter != file.endName(); ++nameIter)
-    // {
-    //     // Read in only the grid we are interested in.
-    //     // if (nameIter.gridName() == "LevelSetSphere") {
-    //     //     baseGrid = file.readGrid(nameIter.gridName());
-    //     // }
-    //     // else {
-    //         std::cout << "skipping grid " << nameIter.gridName() << std::endl;
-    //     // }
-    // }
-    // file.close();
+    file.open();
+    // Loop over all grids in the file and retrieve a shared pointer
+    // to the one named "LevelSetSphere".  (This can also be done
+    // more simply by calling file.readGrid("LevelSetSphere").)
+    openvdb::GridBase::Ptr baseGrid;
+    for (openvdb::io::File::NameIterator nameIter = file.beginName();
+         nameIter != file.endName(); ++nameIter)
+    {
+        // Read in only the grid we are interested in.
+        if (nameIter.gridName() == "density")
+        {
+            baseGrid = file.readGrid(nameIter.gridName());
+            printf("loading grid %s (%s)\n", nameIter.gridName().c_str(), baseGrid->type().c_str());
+        }
+        else
+        {
+            std::cout << "skipping grid " << nameIter.gridName() << std::endl;
+        }
+    }
+    file.close();
 
-    // // From the example above, "LevelSetSphere" is known to be a FloatGrid,
-    // // so cast the generic grid pointer to a FloatGrid pointer.
-    // openvdb::FloatGrid::Ptr grid = openvdb::gridPtrCast<openvdb::FloatGrid>(baseGrid);
-    // // Convert the level set sphere to a narrow-band fog volume, in which
-    // // interior voxels have value 1, exterior voxels have value 0, and
-    // // narrow-band voxels have values varying linearly from 0 to 1.
-    // const float outside = grid->background();
-    // const float width = 2.0 * outside;
-    // // Visit and update all of the grid's active values, which correspond to
-    // // voxels on the narrow band.
-    // for (openvdb::FloatGrid::ValueOnIter iter = grid->beginValueOn(); iter; ++iter) {
+    // From the example above, "LevelSetSphere" is known to be a FloatGrid,
+    // so cast the generic grid pointer to a FloatGrid pointer.
+    openvdb::FloatGrid::Ptr grid = openvdb::gridPtrCast<openvdb::FloatGrid>(baseGrid);
+
+    // Return the number of active voxels. More...
+    auto g_count = grid->activeVoxelCount();
+    std::cout << "activeVoxelCount " << g_count << std::endl;
+
+    // Return the axis-aligned bounding box of all active voxels. More...
+    auto g_bbox = grid->evalActiveVoxelBoundingBox();
+    std::cout << "evalActiveVoxelBoundingBox " << g_bbox << std::endl;
+
+    // Return the dimensions of the axis-aligned bounding box of all active voxels. More...
+    auto g_dims = grid->evalActiveVoxelDim();
+    std::cout << "evalActiveVoxelDim " << g_dims << std::endl;
+    std::cout << "evalActiveVoxelDim - count " << (size_t)g_dims.x() * g_dims.y() * g_dims.z() << std::endl;
+    std::cout << "activeVoxelCount / count " << (float)g_count / ((size_t)g_dims.x() * g_dims.y() * g_dims.z()) << std::endl;
+
+    // Return the number of bytes of memory used by this grid.
+    auto g_ram = grid->memUsage();
+    std::cout << "memUsage " << g_ram << "(Bytes)" << std::endl;
+
+    // Visit and update all of the grid's active values, which correspond to
+    // voxels on the narrow band.
+    // for (openvdb::FloatGrid::ValueOnIter iter = grid->beginValueOn(); iter; ++iter)
+    // {
     //     float dist = iter.getValue();
-    //     iter.setValue((outside - dist) / width);
-    // }
-    // // Visit all of the grid's inactive tile and voxel values and update the values
-    // // that correspond to the interior region.
-    // for (openvdb::FloatGrid::ValueOffIter iter = grid->beginValueOff(); iter; ++iter) {
-    //     if (iter.getValue() < 0.0) {
-    //         iter.setValue(1.0);
-    //         iter.setValueOff();
+    //     if (iter.isVoxelValue())
+    //     {
+    //         printf("[value] %f ", dist);
+    //         printf("VOXEL ");
+    //         // indices in the index space
+    //         std::cout << iter.getCoord() << std::endl;
+    //     }
+    //     else
+    //     {
+    //         printf("[value] %f ", dist);
+    //         printf("TILE ");
+    //         // bounding boxes in the index space
+    //         openvdb::CoordBBox bbox;
+    //         iter.getBoundingBox(bbox);
+    //         std::cout << bbox << std::endl;
     //     }
     // }
-    // // Set exterior voxels to 0.
-    // openvdb::tools::changeBackground(grid->tree(), 0.0);
 
     return 0;
 }
